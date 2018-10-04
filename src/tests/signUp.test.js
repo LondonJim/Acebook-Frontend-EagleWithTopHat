7079f35fc558d6122314505b3b9a0c3731564796
Samir Soormally <samirsoormally@gmail.com>
2018-10-04 11:11:41 +0100

    tests: adds snapshot tests for signup

--- src/tests/signUp.test.js
+++ src/tests/signUp.test.js
@@ -3,10 +3,31 @@ import SignUp from '../components/signUp';
 import {shallow, mount, render} from 'enzyme'
 
 describe('signUp', () => {
-  it('starts with empty full name', () => {
-    const wrapper = shallow(<SignUp />)
-    const fullNameState = wrapper.state().fullName
+  const wrapper = shallow(<SignUp />)
 
-    expect(fullNameState).toEqual("")
+  // it('should render without throwing an error', () => {
+  //   expect(wrapper.find('form.signup').toBe(true)
+  // })
+
+  describe('fullName', () => {
+    const fullNameState = wrapper.state().fullName
+    it('starts with empty full name', () => {
+
+      expect(fullNameState).toEqual("")
+    })
+
+    it('recognises keyboard input', () => {
+      wrapper.find('[name="fullName"]').simulate('click')
+
+      expect(wrapper).toMatchSnapshot();
+    })
+
+
+    it('changes state on keyboard input', () => {
+      wrapper.find('#fullName').simulate('change', { target: {name:'fullName', value: 'fakeemail@fakeemail.com' }})
+
+      expect(wrapper.state('fullName')).toEqual('fakeemail@fakeemail.com')
+    })
   })
+
 })
