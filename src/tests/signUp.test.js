ef254d13e2fb1af7b21895a4c074f25a88a86689
Samir Soormally <samirsoormally@gmail.com>
2018-10-04 11:43:43 +0100

    api fetch test written

--- src/tests/signUp.test.js
+++ src/tests/signUp.test.js
@@ -5,14 +5,9 @@ import {shallow, mount, render} from 'enzyme'
 describe('signUp', () => {
   const wrapper = shallow(<SignUp />)
 
-  // it('should render without throwing an error', () => {
-  //   expect(wrapper.find('form.signup').toBe(true)
-  // })
-
   describe('fullName', () => {
     const fullNameState = wrapper.state().fullName
-    it('starts with empty full name', () => {
-
+    it('starts empty', () => {
       expect(fullNameState).toEqual("")
     })
 
@@ -22,11 +17,67 @@ describe('signUp', () => {
       expect(wrapper).toMatchSnapshot();
     })
 
+    it('changes state on keyboard input', () => {
+      wrapper.find('[name="fullName"]').simulate('change', { target: {name:'fullName', value: 'Fake Name' }})
+
+      expect(wrapper.state('fullName')).toEqual('Fake Name')
+    })
+  })
+
+  describe('email', () => {
+    const emailState = wrapper.state().email
+    it('starts empty', () => {
+      expect(emailState).toEqual("")
+    })
+
+    it('recognises keyboard input', () => {
+      wrapper.find('[name="email"]').simulate('click')
+
+      expect(wrapper).toMatchSnapshot();
+    })
 
     it('changes state on keyboard input', () => {
-      wrapper.find('#fullName').simulate('change', { target: {name:'fullName', value: 'fakeemail@fakeemail.com' }})
+      wrapper.find('[name="email"]').simulate('change', { target: {name:'email', value: 'fakeemail@fakeemail.com' }})
 
-      expect(wrapper.state('fullName')).toEqual('fakeemail@fakeemail.com')
+      expect(wrapper.state('email')).toEqual('fakeemail@fakeemail.com')
+    })
+  })
+
+  describe('password', () => {
+    const passwordState = wrapper.state().password
+    it('starts empty', () => {
+      expect(passwordState).toEqual("")
+    })
+
+    it('recognises keyboard input', () => {
+      wrapper.find('[name="password"]').simulate('click')
+
+      expect(wrapper).toMatchSnapshot();
+    })
+
+    it('changes state on keyboard input', () => {
+      wrapper.find('[name="password"]').simulate('change', { target: {name:'password', value: 'fakepassword' }})
+
+      expect(wrapper.state('password')).toEqual('fakepassword')
+    })
+  })
+
+  describe('passwordConfirmation', () => {
+    const passwordConfirmationState = wrapper.state().passwordConfirmation
+    it('starts empty', () => {
+      expect(passwordConfirmationState).toEqual("")
+    })
+
+    it('recognises keyboard input', () => {
+      wrapper.find('[name="passwordConfirmation"]').simulate('click')
+
+      expect(wrapper).toMatchSnapshot();
+    })
+
+    it('changes state on keyboard input', () => {
+      wrapper.find('[name="passwordConfirmation"]').simulate('change', { target: {name:'passwordConfirmation', value: 'fakepasswordConfirmation' }})
+
+      expect(wrapper.state('passwordConfirmation')).toEqual('fakepasswordConfirmation')
     })
   })
 
